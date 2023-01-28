6233f127b612b0b2536b91cc9d830f9579fde64f
AndreConforti <andre.r.m.conforti@gmail.com>
2023-01-28 20:47:08 -0300

    Criada a classe Bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -13,13 +14,17 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     ## Cria uma espaçonave
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+
+    ## Cria um grupo no qual serão armazenados os projéteis
+    bullets = Group()
 
     ## Inicia o laço principal do jogo
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
--- ship.py
+++ ship.py
@@ -4,9 +4,10 @@ import pygame
 class Ship():
     """Uma classe para referenciar um espaçonave"""
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """inicializa a espaçonave e define sua posição inicial"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         ## Carrega a imagem da espaçonave e obtém seu rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -17,14 +18,24 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        ## Flag de movimento
-        self.moving_right = False
+        ## Armazena um valor decimal para o centro da espaçonave
+        self.center = float(self.rect.centerx)
 
+        ## Flags de movimento
+        self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        """Atualiza a posição da espaçonave de acordo com a flag de movimento"""
-        if self.moving_right:
-            self.rect.centerx += 1
+        """Atualiza a posição da espaçonave de acordo com as flags de movimento"""
+
+        ## Atualiza o valor do centro da espaçonave, e não o retângulo
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        ## Atualiza o objeto rect de acordo com o self.center
+        self.rect.centerx = self.center
     
     def blitme(self):
         """Desenha a espaçonave em sua posição atual"""
